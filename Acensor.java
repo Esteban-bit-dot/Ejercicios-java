0af662c27723c94a522cb02c0a4b2f2257c83c9e
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-14 08:44:40 -0500

    Create Acensor.java

---
+++ Acensor.java
@@ -0,0 +1,27 @@
+package ejercicio.java;
+
+import java.util.Scanner;
+
+
+public class Acensor {
+
+    public static void Acensor(String[] args) {
+          Acensor();  
+    }
+    public static void Acensor(){
+        String pisos="10";
+        Scanner TomarDatos = new Scanner(System.in);
+        System.out.print("ingrese el piso al que desea ir:  ");
+        int numero = TomarDatos.nextInt();
+        TomarDatos.close();
+        Scanner ProcesarDatos = new Scanner(System.in);
+        int numero = ProcesarDatos.nextInt();
+        ProcesarDatos.close();
+        if(numero==10){
+            System.out.println("el piso es valido");
+        }else{
+            System.out.println("el piso es invalido");
+        }
+
+    }
+  }
