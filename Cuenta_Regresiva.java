c717af3521368831edfd8c7a8ed0540a87886994
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-17 08:48:13 -0500

    Create Cuenta_Regresiva.java

---
+++ Cuenta_Regresiva.java
@@ -0,0 +1,26 @@
+package ejercicios.java;
+
+public class Cuenta_regresiva {
+    
+    public static void main(String[]args){
+     Numero();
+ }
+    public static String Numero(){
+     String numero = "";
+     Scanner TomarDatos new Scanner(System.in);
+     Scanner dato_Numero = new Scanner(System.in);
+     System.out.print("ingrese un numero: ");
+     Numero = dato_Numero.nextInt();
+     TomarDatos.close();
+     Scanner ProcesarDatos = new Scanner(System.in);
+     int Numero = ProcesarDatos.nextInt();
+     ProcesarDatos.close();
+     while(numero){
+        boolean num = true;
+        System.out.println(num + : + 0 + 0);
+        System.out.println("desea ingresar otro numero: ");
+        Numero = dato_Numero.nextInt();
+     } 
+     return null;
+  }
+}
