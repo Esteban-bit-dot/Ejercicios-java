bcb9fb89aee6197f2a63451b8a6dd8d64b6f3732
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-17 09:53:24 -0500

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,36 @@
+package ejercicios.java;
+
+import java.util.Scanner;
+
+
+public class Menu {
+     public static void main(String[]args){
+     Menu();
+ }
+     public static String Menu(){
+       String Menu = "";
+       Scanner TomarDatos = new Scanner(System.in);
+       Menu = dato_Menu.nextInt();
+       TomarDatos.close();
+       Scanner ProcesarDatos = new Scanner(System.in);
+       Menu = ProcesarDatos.nextInt();
+       ProcesarDatos.close();
+       do {
+         int texto = 0;
+         int precio_Total= 0;
+         int Acumulador = texto + 1;
+         System.out.println("***Menu de Comida***");
+         System.out.println(" 1. pollo frito $20.000 ");
+         System.out.println(" 2. salchipapa $15.000");
+         System.out.println("3. churro $6.000");
+         System.out.println("4. pasteles $5.000");
+         System.out.println("5. papa rellena $8.000");
+         System.out.print("Seleccione un menu: ");
+         String Precio_total = Menu();
+         Menu = dato_Menu.nextInt();
+     }while(Menu>5){
+         
+     }
+       return null;
+   }
+}
