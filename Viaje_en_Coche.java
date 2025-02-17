6a79a119c7f618e3fb33b92070e2edbcf6a71414
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-17 08:31:09 -0500

    Create Viaje_en_Coche.java

---
+++ Viaje_en_Coche.java
@@ -0,0 +1,27 @@
+package ejercicios.java;
+
+public class Viaje_en_Coche {
+    
+     public static void main(String[]args){
+     viaje();
+ }
+    public static String viaje(){
+     String Velocidad = "";
+     String Distancia = "";
+     Scanner TomarDatos = new Scanner(System.in);
+     Scanner dato_Viaje = new scanner(System.in); 
+     System.out.print("ingrese la velocidad: ");
+     Velocidad = dato_Velocidad.nextInt();
+     System.out.print("ingrese la distacia: ");
+     Distancia = dato_Distancia.nextInt();
+     TomarDatos.close();
+     Scanner ProcesarDatos = new Scanner(System.in);
+     int Producto = ProcesarDatos.nextInt();
+     ProcesarDatos.close();
+     while(t=d/v){
+        System.out.println("¿desea hace mas viajes?: ");
+        Viaje = dato_Viaje.nextInt();
+     } 
+     return null;
+  }
+}
