58e16a745febc213026e5151772974c2680a203a
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-18 09:06:00 -0500

    Create Suma.java

---
+++ Suma.java
@@ -0,0 +1,32 @@
+
+package ejecicios.java;
+
+import java.util.Scanner;
+
+public class Suma {
+    public static String Numeros(int Mostrar_Contactos){
+        String Numeros  = "";
+        Scanner TomarDatos = new Scanner(System.in);
+        int Numeros = TomarDatos.nextInt();
+        TomarDatos.close();
+        Scanner ProcesarDatos = new Scanner(System.in);
+        Numeros = ProcesarDatos.nextInt();
+        ProcesarDatos.close();
+        System.out.print("ingrese el primer numero: ");
+        System.out.print("ingrese el segundo numero: ");
+        Numeros = dato_Numeros.nextInt();
+        int Acumulador = Numeros + 1;
+        int Mostrar_suma = 0;
+        int i;
+        int num = 0;
+        for(i=1; 1<=num, i++){
+            int num1 = 0;
+            int num2 = 0;
+           int Numero = num1 + num2;
+           System.out.print(Mostrar_suma);
+        }
+        return null;
+}
+
+}
+    
