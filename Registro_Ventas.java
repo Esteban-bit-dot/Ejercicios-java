30733e6cf0025bd5b3166fd976d3a5f11eee4dee
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-18 07:47:38 -0500

    Create Registro_Ventas.java

---
+++ Registro_Ventas.java
@@ -0,0 +1,26 @@
+package ejecicios.java;
+
+import java.util.Scanner;
+
+public class Registro_Ventas {
+    public static String Ventas(){
+        String Registro = "";
+        Scanner TomarDatos = new Scanner(System.in);
+        Registro = dato_Registro.nextInt();
+        TomarDatos.close();
+        Scanner ProcesarDatos = new Scanner(System.in);
+        Registro= dato_Registro.nextInt();
+        ProcesarDatos.close();
+        int Registro = 0;
+        do{
+           System.out.println("ingrese un producto con su precio($): ");
+           int Acumulador = Registro + 1;
+        }while (Registro + 1){
+             System.out.print("¿desea hacer gregar mas Registros?: ");
+            int productos_Vendidos = 0;
+            int Monto = productos_Vendidos;
+             System.out.print(Monto);
+        }
+      return null;
+    }
+}
