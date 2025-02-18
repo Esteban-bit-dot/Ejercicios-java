bb05cebb8309d8d943d95b20819eb9af138dd639
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-18 08:55:25 -0500

    Create Agenda_Contactos.java

---
+++ Agenda_Contactos.java
@@ -0,0 +1,28 @@
+
+package ejecicios.java;
+
+import java.util.Scanner;
+
+public class Agenda_Contactos {
+
+    public static String Contactos(int Mostrar_Contactos){
+        String Contactos  = "";
+        Scanner TomarDatos = new Scanner(System.in);
+        int Operacion_compra = TomarDatos.nextInt();
+        TomarDatos.close();
+        Scanner ProcesarDatos = new Scanner(System.in);
+        Operacion_compra = ProcesarDatos.nextInt();
+        ProcesarDatos.close();
+        System.out.print("ingrese el nombre del contacto: ");
+        System.out.print("ingrese el numero de telefono: ");
+        Contactos = dato_Contactos.nextInt();
+        int Acumulador = Contactos + 1;
+        int Registrar_Contactos = 0;
+        int i;
+        for(i=1; 1<=Contactos, i++){
+           int Registrar = 0;
+           System.out.print(Mostrar_Contactos);
+        }
+        return null;
+   }
+}
