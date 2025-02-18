805bbca0b8f5985f1450a79c3a3b090208b13688
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-18 07:34:56 -0500

    Create Gestion_Tareas.java

---
+++ Gestion_Tareas.java
@@ -0,0 +1,24 @@
+package ejecicios.java;
+
+import java.util.Scanner;
+
+public class Tarea {
+    public static String descripcion(){
+        String Descripcion = "";
+        Scanner TomarDatos = new Scanner(System.in);
+        Descripcion = dato_Descripcion.nextInt();
+        TomarDatos.close();
+        Scanner ProcesarDatos = new Scanner(System.in);
+        Descripcion= dato_Descripcion.nextInt();
+        ProcesarDatos.close();
+        int descripcion = 0;
+        do{
+            int Acumulador = descripcion + 1;
+           System.out.println("ingrese el nombre de la tarea: ");
+           System.out.println("ingrese la fecha de vencimiento: ");
+        }while (descripcion + 1){
+             System.out.print("¿desea hacer gregar mas tareas?: ");
+        }
+      return null;  
+    } 
+}
