0194028acacbe44c4993643ae4e647cb1268a003
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-18 07:19:57 -0500

    Create Resultado_Examen.java

---
+++ Resultado_Examen.java
@@ -0,0 +1,33 @@
+package ejecicios.java;
+
+import java.util.Scanner;
+
+public class Resultado_Examen {
+    
+    public static void main(String[] args) {
+        Sensor();
+    }
+    public static String Sensor(){
+        String Resultado = "";
+        Scanner TomarDatos = new Scanner(System.in);
+        Resultado = dato_Resultado.nextInt();
+        TomarDatos.close();
+        Scanner ProcesarDatos = new Scanner(System.in);
+        Resultado = dato_Resultado.nextInt();
+        ProcesarDatos.close();
+        boolean Reprobado = false;
+        do{
+          var Resultado = 0;
+          if (Resultado){
+            System.out.print("Ingrese la nota del Aprendiz: ");
+              boolean Calcular_promedio = true;
+            System.out.print(Calcular_promedio);
+          }else{
+            System.out.print("Error");
+          }
+        }while(Reprobado){
+           System.out.print("Ingrese de nuevo la nota: ");
+        }
+      return null;  
+    } 
+}
