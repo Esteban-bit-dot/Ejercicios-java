ac4fb322b7f542421df851e6998ebbceff5b0770
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-20 08:20:20 -0500

    Update Cajero_Automatico.java

--- Cajero_Automatico.java
+++ Cajero_Automatico.java
@@ -9,20 +9,20 @@ public class Cajero_Automatico {
        String cajero();
     }
     public static String cajero(){
-     String Saldo = "1.000000";
+     double Saldo = "1.000000";
      String dato_saldo = "";
      Scanner TomarDatos new Scanner(System.in);
      Scanner dato_Saldo = new Scanner(System.in);
      System.out.print("ingrese el monto de desea retirar: ");
-     Saldo = dato_saldo.nextInt();
+     Saldo = dato_saldo.nextDouble();
      TomarDatos.close();
      Scanner ProcesarDatos = new Scanner(System.in);
-     int Saldo = ProcesarDatos.nextInt();
+     double Saldo = ProcesarDatos.nextDouble();
      int Monto;
      while(Monto>Saldo){
         System.out.println("El monto es mayor al saldo error ");
         System.out.println("ingrese el monto que desea retirar: ");
-        Saldo = dato_Saldo.nextInt();
+        Saldo = dato_Saldo.nextDouble();
      } 
      return null;
   }
