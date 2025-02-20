74af26522ad149f86d94b44c036073f7e5ba556f
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-20 08:55:01 -0500

    Update Tienda2.java

--- Tienda2.java
+++ Tienda2.java
@@ -8,20 +8,20 @@ public class Tienda2 {
     public static void main(String[]args){
      tienda();
  }
-   public static String tienda(){
+   public  String tienda(){
      String Producto = "15";
-     String precio = "15.000";
+     double precio = "15.000";
      Scanner TomarDatos new Scanner(System.in);
      Scanner dato_Producto = new Scanner(System.in);
      System.out.print("ingrese la cantidad de productos que desea comprar: ");
-     producto = dato_Producto.nextInt();
+     int producto = dato_Producto.nextInt();
      TomarDatos.close();
      Scanner ProcesarDatos = new Scanner(System.in);
      int Producto = ProcesarDatos.nextInt();
      ProcesarDatos.close();
-     int precio;
+     double precio = "";
      while(Producto){
-        boolean Calcular_precio;
+        int Calcular_precio;
         System.out.println(Calcular_precio);
         System.out.println("desea agregar otro producto productos : ");
         Producto = dato_Producto.nextInt();
