9aa9fa811b6be2db087054fb7778279cd91acc34
Esteban-bit-dot <eandrescardales@gmail.com>
2025-02-20 08:01:52 -0500

    Update Acensor_Sensor_temperatura_Umbral_Alerta.java

--- Acensor_Sensor_temperatura_Umbral_Alerta.java
+++ Acensor_Sensor_temperatura_Umbral_Alerta.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class Acesor_Sensor_Temperatura_Umbral_Alerta {
 
-        public static void main(String[] args) {
-        }
-        public static String temperatura(){
+        public static void temperatura(String[] args) {
         Scanner TomarDatos = new Scanner(System.in);
         System.out.print("Ingrese la temperatura ambiente (°C): ");
         int temperatura = TomarDatos.nextInt();
+        int Sensor = dato_Sensor.nextInt();
         // Rango de temperatura aceptable
         int temp = 25;
         TomarDatos.close();
         Scanner ProcesarDatos = new Scanner(System.in);
         int temperatura = ProcesarDatos.nextInt();
+        int Sensor = dato_Sensor.nextInt();
         ProcesarDatos.close();
         if (temperatura <= temp) {
             // Si la temperatura está dentro del rango, el ascensor puede moverse
             System.out.print("Ingrese el piso deseado: ");
             
-        } else {
+        } else(Alerta=temp) {
             // Si la temperatura está fuera del rango, mostrar mensaje y no moverse
             System.out.println("Alerta La temperatura no es adecuada para mover el ascensor.");
         }
